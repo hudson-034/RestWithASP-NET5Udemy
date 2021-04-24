f888d4a63d7313b548cd2098c9970c10efa516a1
hudson <rocha.hudson@hotmail.com>
2021-04-24 13:08:38 -0300

    Implementation principal verbs

---
+++ 03_RestWithASPNETUdemy_Scaffold_Verbos/Verbos/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Verbos.Model;
+
+namespace Verbos.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
